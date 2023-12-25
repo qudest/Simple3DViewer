295c9ce60537a78bfdb0ad6084648c37b8a33a9c
nagornyy_a_s <nagornyi.arsenii@mail.ru>
2023-12-26 00:44:44 +0300

    add autoresize

--- src/main/java/com/cgvsu/Simple3DViewer.java
+++ src/main/java/com/cgvsu/Simple3DViewer.java
@@ -16,8 +16,8 @@ public class Simple3DViewer extends Application {
         AnchorPane viewport = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("fxml/gui.fxml")));
 
         Scene scene = new Scene(viewport);
-        stage.setMinWidth(1600);
-        stage.setMinHeight(900);
+        stage.setMinWidth(640);
+        stage.setMinHeight(480);
         viewport.prefWidthProperty().bind(scene.widthProperty());
         viewport.prefHeightProperty().bind(scene.heightProperty());
 
